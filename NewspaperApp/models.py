99b230b46e2b72370a15c1402bdeca65ba9b406b
Usama Jamil <usamajamil77@gmail.com>
2018-08-25 00:29:49 +0500

    Record url Changes

--- NewspaperApp/models.py
+++ NewspaperApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class RequestStore(models.Model):
+    request_time = models.DateTimeField(null=True)
+    request_url = models.CharField(max_length=100,default=None)
+    request_method = models.CharField(max_length=20,default=None,null=True,blank=True)
+    status_code = models.IntegerField(default=200,null=True,blank=True)
